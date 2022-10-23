1b89d9de9aca22c0be00165a67407a12ced722a3
VolodyaLev <vladimirlevitsky@yandex.ru>
2022-10-23 23:10:24 +0300

    Added algos directory

---
+++ algos/bubble_sort.py
@@ -0,0 +1,6 @@
+def sort(values):
+    n = len(values)
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if values[j] > values[j + 1]:
+                values[j], values[j + 1] = values[j + 1], a[j]
