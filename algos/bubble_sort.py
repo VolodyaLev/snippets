4119edeb891969ab14ac5f90c512c67ea26c5e85
Vasilii Kuimov <vasilykuimov@yandex.ru>
2022-10-30 14:48:05 +0300

    [SF-45] Поправлен тест

--- algos/bubble_sort.py
+++ algos/bubble_sort.py
@@ -1,6 +1,6 @@
-def sort(values):
+def bubble_sort(values):
     n = len(values)
     for i in range(n - 1):
         for j in range(n - i - 1):
             if values[j] > values[j + 1]:
-                values[j], values[j + 1] = values[j + 1], a[j]
+                values[j], values[j + 1] = values[j + 1], values[j]
